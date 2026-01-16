19170740949b1fb0bc98e56b2309666b4fae62c6
chinmayi2005-07-05 <chinmyiabadar@gmail.com>
2026-01-16 22:30:49 +0530

    is code is Nested if...else...sttement

---
+++ gate.py
@@ -0,0 +1,17 @@
+num = 20
+#nested if --> one if can contain another if.....
+if num > 8:
+    print("number is really grater than 8")
+    if num<= 15:
+        print("number is between 8 and 15")
+
+num = 23
+if num >12:
+    print("hello")
+    #nested if-else statement...
+    if num > 11:
+        print("hi")
+    else :
+        pass
+else:
+    pass
